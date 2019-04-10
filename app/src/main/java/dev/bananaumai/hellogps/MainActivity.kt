51ecf7e3848872b1a0e58f57caa6bd97af99677d
Yuta Shimakawa <banana.umai@gmail.com>
2019-04-10 19:22:50 +0900

    track location update

--- app/src/main/java/dev/bananaumai/hellogps/MainActivity.kt
+++ app/src/main/java/dev/bananaumai/hellogps/MainActivity.kt
@@ -5,26 +5,35 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
-import android.widget.Toast
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationSettingsClient: SettingsClient
+    private lateinit var locationCallback: LocationCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationSettingsClient = LocationServices.getSettingsClient(this)
+        locationCallback = createLocationCallback()
 
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            listenLastLocation()
+            startLocationTracking()
         } else {
             requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1000)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopLocationTracking()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -32,12 +41,58 @@ class MainActivity : AppCompatActivity() {
 
         if (requestCode == 1000) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                listenLastLocation()
+                startLocationTracking()
             }
         }
     }
 
-    private fun listenLastLocation() {
+    private fun startLocationTracking() {
+        lastLocation()
+        val builder = LocationSettingsRequest.Builder()
+        val request = createLocationRequest()
+        //builder.addLocationRequest(request)
+
+        locationSettingsClient.checkLocationSettings(builder.build())
+            .addOnSuccessListener(this) {
+                try {
+                    fusedLocationClient.requestLocationUpdates(request, locationCallback, Looper.myLooper())
+                } catch (e: SecurityException) {
+                    requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1000)
+                    Log.e("bananaumai", e.message)
+                }
+
+            }
+            .addOnFailureListener(this) {
+                throw it
+            }
+    }
+
+    private fun stopLocationTracking() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    private fun createLocationCallback() = object: LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            locationResult ?: return
+            for (location in locationResult.locations){
+                Log.d("bananaumai", "$location")
+            }
+        }
+    }
+
+
+    private fun createLocationRequest(): LocationRequest {
+        val locationRequest = LocationRequest.create() ?: throw RuntimeException("couldn't create locatilon request")
+
+        locationRequest.interval = 10000
+        locationRequest.fastestInterval = 5000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        return locationRequest
+    }
+
+    private fun lastLocation() {
+
         try {
             fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
                 Log.d("bananaumai", "$location")
