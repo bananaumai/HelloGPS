fc95c83dc4ddbb849c4dfc8c58ae8a55bb2d8631
Yuta Shimakawa <banana.umai@gmail.com>
2019-04-10 16:44:34 +0900

    GPS

--- app/src/main/java/dev/bananaumai/hellogps/MainActivity.kt
+++ app/src/main/java/dev/bananaumai/hellogps/MainActivity.kt
@@ -1,12 +1,49 @@
 package dev.bananaumai.hellogps
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            listenLastLocation()
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 1000)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 1000) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                listenLastLocation()
+            }
+        }
+    }
+
+    private fun listenLastLocation() {
+        try {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                Log.d("bananaumai", "$location")
+            }
+        } catch (e: SecurityException) {
+            Log.e("bananaumai", e.message)
+        }
     }
 }
